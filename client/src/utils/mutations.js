6a3ba90e0b284ccf431ed0fef58b2d8e9aec49c1
jaymemichelle <71385722+jaymemichelle@users.noreply.github.com>
2021-11-14 17:29:29 -0500

    started utils files

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,24 @@
+import gql from 'graphql-tag';
+
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+
+export const ADD_USER = gql`
+  mutation addUser($firstName: String!, $lastName: String!, $email: String!, $password: String!) {
+    addUser(firstName: $firstName, lastName: $lastName, email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
