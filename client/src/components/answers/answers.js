e6bc1609a365e7aca84352773562f4d0027b42cb
Lola <lia1m@aol.com>
2021-11-15 17:47:25 -0500

    modified nav & helpers file. removed unused variables and imports.

---
+++ client/src/components/answers/answers.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useStoreContext } from "../../utils/GlobalState";
+
+function Question(quiz) {
+  const [state] = useStoreContext();
+
+  const {
+    question,
+    answer,
+    _id,
+  } = quiz;
+
+  function filterQuestions() {
+    if (!currentCategory) {
+      return state.questions;
+    }
+
+    return state.questions.filter(question => question.category._id === currentCategory);
+  }    
+
+  return (
+    <div className="card px-1 py-1">
+      <Link to={`/questions/${_id}`}>
+        <p>{question, answer}</p>
+      </Link>
+    </div>
+  );
+}
+
+export default Question;
