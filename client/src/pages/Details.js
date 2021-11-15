a6c10b52bff5ce5c224d27223a827f59d21ba397
Lola <lia1m@aol.com>
2021-11-15 17:52:24 -0500

    added home.js page for Charles to work on. Incorporated Detail.js page. Testing code now.

---
+++ client/src/pages/Details.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { useQuery } from '@apollo/react-hooks';
+
+import { useQuestionContext } from "../utils/GlobalState";
+import {UPDATE_QUESTIONS} from "../utils/actions";
+import { idbPromise } from "../utils/helpers";
+
+function Detail() {
+  const [state, dispatch] = useQuestionContext();
+  const { id } = useParams();
+
+  const [currentQuestion, setCurrentQuestion] = useState({});
+
+  const { loading, data } = useQuery(QUERY_PRODUCTS);
+
+  const { questions} = state;
+
+  useEffect(() => {
+    // already in global store
+    if (questions.length) {
+      setCurrentQuestion(questions.find(question => question._id === id));
+    } 
+    // retrieved from server
+    else if (data) {
+      dispatch({
+        type: UPDATE_QUESTIONS,
+        questions: data.questions
+      });
+
+      data.questions.forEach((question) => {
+        idbPromise('questions', 'put', question);
+      });
+    }
+    // get cache from idb
+    else if (!loading) {
+      idbPromise('questions', 'get').then((indexedQuestions) => {
+        dispatch({
+          type: UPDATE_QUESTIONS,
+          products: indexedQuestions
+        });
+      });
+    }
+  }, [products, data, loading, dispatch, id]);
+
+
+  return (
+    <>
+      {currentQuestion ? (
+        <div className="container my-1">
+          <Link to="/">
+            ← Back to Home
+          </Link>
+
+          <h2>{currentQuestion.question}</h2>
+
+          <p>
+            {currentQuestion.answer}
+          </p>
+          
+        </div>
+      ) : null}
+    </>
+  );
+};
+
+export default Detail;
