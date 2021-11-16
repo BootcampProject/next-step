090d792a90232ddb564b00e32beb1d64bd9c99d1
Noyemi Ohanyan <nomiohanyan@gmail.com>
2021-11-16 18:43:53 -0500

    added the about me component & css

---
+++ client/src/components/AboutUs/About.js
@@ -0,0 +1,70 @@
+const AboutUs = () => {
+  return (
+    <div>
+      <div class="cardContainerAbout">
+        <CardGroup>
+          <Card style={{ width: "20rem" }}>
+            <Card.Body>
+              <Card.Title>Lia Mattacchione</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Back and Front-end integrator
+              </Card.Subtitle>
+              <Card.Link href="#">
+                {" "}
+                <BsGithub />{" "}
+              </Card.Link>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: "20rem" }}>
+            <Card.Body>
+              <Card.Title>Sallam Thabet</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Back and Front-end integrator
+              </Card.Subtitle>
+              <Card.Link href="#">
+                {" "}
+                <BsGithub />{" "}
+              </Card.Link>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: "20rem" }}>
+            <Card.Body>
+              <Card.Title>Jayme Winick</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Back and Front-end integrator
+              </Card.Subtitle>
+              <Card.Link href="#">
+                {" "}
+                <BsGithub />{" "}
+              </Card.Link>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: "20rem" }}>
+            <Card.Body>
+              <Card.Title>Charles Dawkins</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Back and Front-end integrator
+              </Card.Subtitle>
+              <Card.Link href="#">
+                {" "}
+                <BsGithub />{" "}
+              </Card.Link>
+            </Card.Body>
+          </Card>
+          <Card style={{ width: "20rem" }}>
+            <Card.Body>
+              <Card.Title>Noyemi Ohanyan</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                Back and Front-end integrator
+              </Card.Subtitle>
+              <Card.Link href="#">
+                {" "}
+                <BsGithub />{" "}
+              </Card.Link>
+            </Card.Body>
+          </Card>
+        </CardGroup>
+      </div>
+    </div>
+  );
+};
