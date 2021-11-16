4eeff4318bceb15e3b07dff67a6866f97287c257
Lola <lia1m@aol.com>
2021-11-16 17:54:17 -0500

    had to re-arrange HomePage location within filestructure as well as about us.

--- client/src/App.js
+++ client/src/App.js
@@ -31,7 +31,6 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-    
         <QuizQuestions>
             <NavBar />
             <main>
--- client/src/components/AboutUs/About.js
+++ client/src/components/AboutUs/About.js
@@ -6,6 +6,7 @@ import './about.css';
 const AboutUs = () => {
   return (
       <div>
+          <h2 class="headerAbout">About Us</h2>
           <div class="cardContainerAbout">
             <CardGroup >
                 <Card style={{ width: '20rem' }}>
@@ -17,28 +18,28 @@ const AboutUs = () => {
                 </Card>
                 <Card style={{ width: '20rem' }}>
                     <Card.Body>
-                        <Card.Title>Lia Mattacchione</Card.Title>
+                        <Card.Title>Sallam Thabet</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">Back and Front-end integrator</Card.Subtitle>
                         <Card.Link href="#"> <BsGithub /> </Card.Link>
                     </Card.Body>
                 </Card>
                 <Card style={{ width: '20rem' }}>
                     <Card.Body>
-                        <Card.Title>Lia Mattacchione</Card.Title>
+                        <Card.Title>Jayme Winick</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">Back and Front-end integrator</Card.Subtitle>
                         <Card.Link href="#"> <BsGithub /> </Card.Link>
                     </Card.Body>
                 </Card>
                 <Card style={{ width: '20rem' }}>
                     <Card.Body>
-                        <Card.Title>Lia Mattacchione</Card.Title>
+                        <Card.Title>Charles Dawkins</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">Back and Front-end integrator</Card.Subtitle>
                         <Card.Link href="#"> <BsGithub /> </Card.Link>
                     </Card.Body>
                 </Card>
                 <Card style={{ width: '20rem' }}>
                     <Card.Body>
-                        <Card.Title>Lia Mattacchione</Card.Title>
+                        <Card.Title>Noyemi Ohanyan</Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">Back and Front-end integrator</Card.Subtitle>
                         <Card.Link href="#"> <BsGithub /> </Card.Link>
                     </Card.Body>
