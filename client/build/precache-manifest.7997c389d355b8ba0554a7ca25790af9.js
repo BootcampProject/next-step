7d48529fff5ed4055d201f4c3c6d20f38a4ced64
jaymemichelle <71385722+jaymemichelle@users.noreply.github.com>
2021-11-19 20:43:27 -0500

    final

---
+++ client/build/precache-manifest.7997c389d355b8ba0554a7ca25790af9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "524fd2f7ff13e6da5748e8992b857f4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a77c59aeacc6cf7e0806",
+    "url": "/static/css/2.fa43bc9e.chunk.css"
+  },
+  {
+    "revision": "b11089e007ac38ff5212",
+    "url": "/static/css/main.39bdc168.chunk.css"
+  },
+  {
+    "revision": "a77c59aeacc6cf7e0806",
+    "url": "/static/js/2.eddc0e66.chunk.js"
+  },
+  {
+    "revision": "9aba2dc1bf36950741eef1477ac2f756",
+    "url": "/static/js/2.eddc0e66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b11089e007ac38ff5212",
+    "url": "/static/js/main.d543da62.chunk.js"
+  },
+  {
+    "revision": "2c5e68dfff0d75264fd6",
+    "url": "/static/js/runtime-main.55958090.js"
+  },
+  {
+    "revision": "ea8f273841a43338a26b08036b36d834",
+    "url": "/static/media/Geography.ea8f2738.png"
+  },
+  {
+    "revision": "ef7adeb914f8e4103ade805bf8640544",
+    "url": "/static/media/History.ef7adeb9.png"
+  },
+  {
+    "revision": "c5989f36a6487fc1b2180d7238e50ac5",
+    "url": "/static/media/Logo.c5989f36.png"
+  },
+  {
+    "revision": "33d26e1aa0fad1623baf9e68b3c300d6",
+    "url": "/static/media/Math.33d26e1a.png"
+  },
+  {
+    "revision": "9fd30c95134325c1190566551e740b60",
+    "url": "/static/media/Science.9fd30c95.png"
+  },
+  {
+    "revision": "ca4404a420f5d47b32e2b09aa1a665fc",
+    "url": "/static/media/aboutheader.ca4404a4.png"
+  },
+  {
+    "revision": "76dbe9aafb081a4ecf5799c6ae00ebf6",
+    "url": "/static/media/hero.76dbe9aa.jpg"
+  }
+]);
